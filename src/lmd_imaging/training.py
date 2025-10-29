a1df3bd3db89d79ab62101eae5ec8af844f1d47e
António Alves <simoesalves09@gmail.com>
2025-10-29 01:51:44 +0000

    Rework dataset structure

--- src/lmd_imaging/training.py
+++ src/lmd_imaging/training.py
@@ -4,7 +4,6 @@ from dataclasses import dataclass
 from pathlib import Path
 from typing import Any
 
-import sklearn
 from ultralytics import YOLO
 
 from .labeling import ManualLabeler, labels_to_txt
@@ -36,7 +35,7 @@ class OutputPaths:
         self.label_validate = output_dir.joinpath("data", "labels", "val")
 
     def create(self, remove_existing: bool = False) -> None:
-        for path in (self.image_train, self.image_validate, self.label_train, self.label_validate):
+        for path in (self.image_train, self.label_train):
             if remove_existing:
                 shutil.rmtree(path)
             elif path.exists():
@@ -48,27 +47,21 @@ class OutputPaths:
 def prepare_dataset_and_generate_labels(
     dataset_dir: Path,
     output_dir: Path,
-    test_size: float = 0.2,
     remove_existing: bool = False,
 ) -> OutputPaths:
-    img_list = define_dataset(dataset_dir, "png")  # TODO: png
-    img_train, img_test = sklearn.model_selection.train_test_split(img_list, test_size=test_size)
+    input_images = define_dataset(dataset_dir, "png")
 
     output_paths = OutputPaths(output_dir)
     output_paths.create(remove_existing)
 
     labeler = ManualLabeler()
-    for input_images, output_image_set, output_label_set in [
-        (img_train, output_paths.image_train, output_paths.label_train),
-        (img_test, output_paths.image_validate, output_paths.label_validate),
-    ]:
-        for image, labels in zip(iter(input_images), labeler.batch_label(input_images)):
-            if labels is None:
-                continue
+    for image, labels in zip(iter(input_images), labeler.batch_label(input_images)):
+        if labels is None:
+            continue
 
-            shutil.copy(image, output_image_set.joinpath(image.name))
-            with output_label_set.joinpath(image.stem + ".txt").open("w", encoding="utf-8") as fd:
-                labels_to_txt(labels, fd)
+        shutil.copy(image, output_paths.image_train.joinpath(image.name))
+        with output_paths.label_train.joinpath(image.stem + ".txt").open("w", encoding="utf-8") as fd:
+            labels_to_txt(labels, fd)
 
     return output_paths
 
@@ -95,7 +88,7 @@ def train_dataset(dataset_paths: OutputPaths, yolo_model: str = "yolo11n-seg.yam
     if "imgsz" not in train_kwargs:
         train_kwargs["imgsz"] = 640
     if "epochs" not in train_kwargs:
-        train_kwargs["epochs"] = 40
+        train_kwargs["epochs"] = 300
 
     model = YOLO(yolo_model)
     model.train(**train_kwargs)
